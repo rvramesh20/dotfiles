924d1ddcbbeaefd082734bd0ad95ab16b1d47467
Ramesh <rvramesh20@gmail.com>
2020-10-24 21:57:48 +0530

    Added Org Mode configurations

--- init.el
+++ init.el
@@ -54,10 +54,57 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-easy-img-insert org-attach-screenshot command-log-mode use-package)))
+   '(org-bullets org-easy-img-insert org-attach-screenshot command-log-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; My Org Mode configuration
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+
+(provide 'emacs-orgmode-config)
+(require 'ox)  
+
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+
+;; ;; GLOBAL Keybindings that can be added to .emacs
+;; (global-set-key [f4] 'org-capture)
+;; (global-set-key [f5] '(lambda () (interactive)(find-file "~/org/mygtd.org")))
+;; (global-set-key [f6] 'org-todo-list)
+;; ;; (global-set-key [f7] 'org-agenda)
+
+
+(setq org-log-done nil) 
+(setq org-startup-truncated nil)
+
+(setq org-directory (expand-file-name "~/MyOrgSpace"))
+(setq org-default-notes-file (concat org-directory "/mygtd.org"))
+(setq org-agenda-files '("~/MyOrgSpace" "~/MyOrgSpace/www/org" "~/MyOrgSpace/xwww/_org"))
+
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+;; Remove the formatting symbols like ** for Bold
+ (setq org-hide-emphasis-markers t)
+
+
+;; Org export options
+'(org-file-apps
+    (quote
+      ((auto-mode . emacs)
+      ("\\.mm\\'" . default)
+      ("\\.x?html?\\'" . "/usr/bin/firefox %s")
+      ("\\.pdf\\'" . default))))
+
