5aa2d933e7e58cc30e9675caba4ea0e9b746a481
Ramesh <rvramesh20@gmail.com>
2020-10-24 20:13:19 +0530

    Added initial version

---
+++ init.el
@@ -0,0 +1,63 @@
+
+;; --------------------------------
+;; My init.el configuration file
+;; Initial version : 0.1
+;; Created on 24-Oct-2020
+;; -- RAMESH
+;; --------------------------------
+
+;; Remove startup message
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+
+;;(menu-bar-mode -1)            ; Disable the menu bar
+
+; Set up the visible bell
+(setq visible-bell t)
+
+;; font settings 
+(set-face-attribute 'default nil :font "Monaco" :height 100)
+
+;;Load theme 
+(load-theme 'wombat)
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package command-log-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(org-easy-img-insert org-attach-screenshot command-log-mode use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
